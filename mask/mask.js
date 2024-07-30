b573c16e74330ab0dcc0b61dda567ccc9cde886a
LagTheSystem <lagthesystem@duck.com>
2024-07-30 12:02:36 -0500

    Update mask.js

--- mask/mask.js
+++ mask/mask.js
@@ -5,7 +5,7 @@
 function mask() {
     const urlInput = document.getElementById('urlInput').value;
     const encodedUrl = btoa(urlInput);
-    const maskUrl = `https://lagthesystem.dev/util/ref?data=${encodedUrl}`;
+    const maskUrl = `https://mask.lagthesystem.dev/?data=${encodedUrl}`;
     document.getElementById('urlInput').value = maskUrl;
     navigator.clipboard.writeText(maskUrl);
 }
