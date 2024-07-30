ade1ccc3516383e799fe8eaa152ace5e02cf6517
LagTheSystem <lagthesystem@duck.com>
2024-07-30 11:57:17 -0500

    Create mask.js

---
+++ mask/mask.js
@@ -0,0 +1,11 @@
+/*
+ * Masks the URL input by encoding it and updating the input field with the masked URL.
+ * Also copies the masked URL to the clipboard.
+*/
+function mask() {
+    const urlInput = document.getElementById('urlInput').value;
+    const encodedUrl = btoa(urlInput);
+    const maskUrl = `https://lagthesystem.dev/util/ref?data=${encodedUrl}`;
+    document.getElementById('urlInput').value = maskUrl;
+    navigator.clipboard.writeText(maskUrl);
+}
